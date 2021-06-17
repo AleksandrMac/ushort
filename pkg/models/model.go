635434516e0ba67d33e5f8643e5d2c8fc5d15a28
AleksandrMac <62098405+AleksandrMac@users.noreply.github.com>
2021-06-17 14:50:32 +0900

    added DataSourceParam(db *config.DB) string

---
+++ pkg/models/model.go
@@ -0,0 +1,15 @@
+package models
+
+import "github.com/AleksandrMac/ushort/pkg/config"
+
+// DataSourceParam формирует строку с параметрами
+func DataSourceParam(db *config.DB) string {
+	param := "?"
+	if len(db.SslMode) > 0 {
+		param += "sslmode=" + db.SslMode + "&"
+	}
+	if len(db.TimeZone) > 0 {
+		param += "timezone=" + db.TimeZone + "&"
+	}
+	return param[:len(param)-1]
+}
