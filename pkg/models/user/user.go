2533c764709ed727edab7e6b889bcbdb5555f856
AleksandrMac <62098405+AleksandrMac@users.noreply.github.com>
2021-06-18 19:53:04 +0900

    user queryes fixed

--- pkg/models/user/user.go
+++ pkg/models/user/user.go
@@ -6,22 +6,19 @@ import (
 
 	"github.com/AleksandrMac/ushort/pkg/config"
 	"github.com/AleksandrMac/ushort/pkg/models"
+	"github.com/google/uuid"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
 type User struct {
-	id       uint64
-	email    string
-	password string
+	Id       uuid.UUID `db:"id"`
+	Email    string    `db:"email"`
+	Password string    `db:"password"`
 }
 
 var shemas = map[string]string{
-	"create": `CREATE TABLE user (
-	id uuid,
-	email var,
-	password varchar);`,
 	"insert": `INSERT INTO user
 	(id, email, password)
 	VALUES ($1, $2, $3);`,
@@ -30,6 +27,10 @@ var shemas = map[string]string{
 	WHERE id = ;`,
 	"delete": ``,
 	"select": ``,
+}
+
+func New() User {
+	return User{}
 }
 
 func CreateTable(c *config.DB) error {
@@ -69,6 +70,78 @@ email text NOT NULL,
 password text NOT NULL);`); err != nil {
 			return err
 		}
+	}
+	return nil
+}
+
+func (u *User) Insert(c *config.DB) (uuid.UUID, error) {
+	db, err := sqlx.Connect(c.Driver, fmt.Sprintf("postgres://%s:%s@%s:%s/%s%s", c.User, c.Password, c.Host, c.Port, c.Name, models.DataSourceParam(c)))
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	defer func() {
+		if err = db.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	u.Id = uuid.New()
+	_, err = db.NamedExec(`INSERT INTO "public"."users" ("id","email","password")
+	VALUES (:id, :email, :password);`, u)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	return u.Id, nil
+}
+
+func Select(c *config.DB) (*[]User, error) {
+	db, err := sqlx.Connect(c.Driver, fmt.Sprintf("postgres://%s:%s@%s:%s/%s%s", c.User, c.Password, c.Host, c.Port, c.Name, models.DataSourceParam(c)))
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err = db.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	users := []User{}
+	err = db.Select(&users, `SELECT * FROM users;`)
+	if err != nil {
+		return nil, err
+	}
+	return &users, nil
+}
+
+func (u *User) SelectWithID(c *config.DB) (*User, error) {
+	db, err := sqlx.Connect(c.Driver, fmt.Sprintf("postgres://%s:%s@%s:%s/%s%s", c.User, c.Password, c.Host, c.Port, c.Name, models.DataSourceParam(c)))
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err = db.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	user := User{}
+	err = db.Get(&user, `SELECT * FROM users WHERE id=$1;`, u.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (u *User) Delete(c *config.DB) error {
+	db, err := sqlx.Connect(c.Driver, fmt.Sprintf("postgres://%s:%s@%s:%s/%s%s", c.User, c.Password, c.Host, c.Port, c.Name, models.DataSourceParam(c)))
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err = db.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	_, err = db.Exec(`DELETE FROM user WHERE id=$1;`, u.Id.String())
+	if err != nil {
+		return err
 	}
 	return nil
 }
