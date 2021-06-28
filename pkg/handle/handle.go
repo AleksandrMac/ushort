b9b417c2a051fddf7a36f5abccd7d9d55f45c890
AleksandrMac <62098405+AleksandrMac@users.noreply.github.com>
2021-06-28 15:55:30 +0900

    add handel "/"

--- pkg/handle/handle.go
+++ pkg/handle/handle.go
@@ -1,8 +1,18 @@
 package handle
 
-import "github.com/go-chi/chi/v5"
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func SetHandlers(r *chi.Mux) {
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		if _, err := w.Write([]byte("Hello")); err != nil {
+			log.Fatal(err)
+		}
+	})
 	setAuthHandlers(r)
 	setUserHandlers(r)
 	setURLHandlers(r)
