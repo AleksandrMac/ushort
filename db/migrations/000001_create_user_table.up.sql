54c37eae5fd6e90d9cb16f12b2e2f83bf7ed46bd
AleksandrMac <62098405+AleksandrMac@users.noreply.github.com>
2021-07-26 09:30:01 +0900

    migrations modified

--- db/migrations/000001_create_user_table.up.sql
+++ db/migrations/000001_create_user_table.up.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS public.user(
     id uuid,
     email text,
     password text NOT NULL,
-    created_at timestamp with time zone,
-    updated_at timestamp with time zone,
+    created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+    updated_at timestamp with time zone NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id),
     CONSTRAINT email UNIQUE (email)
 );
